76ff125e0863367e1ab5b8e2698efe4d9bc2e89a
praskumar002 <praskumar002@gmail.com>
2022-07-28 10:19:47 +0530

    changes

--- src/Services/CreateObjectService.php
+++ src/Services/CreateObjectService.php
@@ -400,6 +400,7 @@ class CreateObjectService
                 $iceCatobject->setBulletPoints($bulletHtml, $this->currentLanguage);
             }
 
+            $this->setProductStoryData($attributeArray['ProductStory'], $iceCatobject);
             $this->createBrandLogo($basicInformation, $iceCatobject);
 
             $this->createReasonsToBuy($attributeArray, $iceCatobject);
@@ -467,6 +468,31 @@ class CreateObjectService
             ];
         }
         $object->setReviews($reviewData);
+    }
+
+    protected function setProductStoryData($ps, $object)
+    {
+        $psData = [];
+        if (empty($ps)) {
+            return;
+        }
+        foreach($ps as $data) {
+            if (empty($data['Language'])) {
+                $data['Language'] = 'en';
+            }
+
+            $data['Language'] = strtolower($data['Language']);
+            $blockData = [
+                $data['Language'] => [
+                    'productStory' => $data['URL']
+                ],
+            ];
+
+            $psData[] = [
+                "localizedfields" => new BlockElement('localizedfields', 'localizedfields', new Localizedfield($blockData))
+            ];
+        }
+        $object->setProductStory($psData);
     }
 
     /**
