1ef03bcbc2d13c58cdabab8aab1650c5a48e9d4a
sid c <sid@mindstask.com>
2023-02-26 21:43:27 +0530

    fix migration

--- src/Migrations/Version20220907163548.php
+++ src/Migrations/Version20220907163548.php
@@ -28,6 +28,21 @@ final class Version20220907163548 extends AbstractMigration
             ADD IF NOT EXISTS `icecat_password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL '
         );
 
+        $this->addSql('CREATE TABLE IF NOT EXISTS `icecat_recurring_import` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `start_datetime` int NOT NULL,
+            `end_datetime` int NOT NULL,
+            `status` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+            `total_records` int NOT NULL,
+            `processed_records` int NOT NULL,
+            `success_records` int NOT NULL,
+            `error_records` int NOT NULL,
+            `not_found_records` int NOT NULL,
+            `forbidden_records` int NOT NULL,
+            `execution_type` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+            PRIMARY KEY (`id`)
+          ) ENGINE=InnoDB AUTO_INCREMENT=277 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;');
+
         $this->addSql(
             'ALTER TABLE `icecat_recurring_import`
             MODIFY `status` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -47,6 +62,24 @@ final class Version20220907163548 extends AbstractMigration
         DROP COLUMN  IF  EXISTS `app_key`,
         DROP COLUMN  IF  EXISTS `icecat_password`
         ');
+
+        // this is done deliberatley because when installing v3 sometimes this table doesn't
+        // get created due to collate issue.
+        // so add it if one is downgrading from v4 to v3
+        $this->addSql('CREATE TABLE IF NOT EXISTS `icecat_recurring_import` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `start_datetime` int NOT NULL,
+            `end_datetime` int NOT NULL,
+            `status` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+            `total_records` int NOT NULL,
+            `processed_records` int NOT NULL,
+            `success_records` int NOT NULL,
+            `error_records` int NOT NULL,
+            `not_found_records` int NOT NULL,
+            `forbidden_records` int NOT NULL,
+            `execution_type` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+            PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=277 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;');
 
         $this->addSql(
             'ALTER TABLE `icecat_recurring_import`
